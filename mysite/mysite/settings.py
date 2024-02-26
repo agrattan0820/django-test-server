21a0c4c9c76b6937920a68dd9de9600af152c704
GameDog9988 <alex@grattan.me>
2024-02-25 19:28:22 -0500

    feat: add allowed hosts

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 DEBUG = os.environ["DEBUG"]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", ".agrattan.com"]
 
 # Application definition
 
